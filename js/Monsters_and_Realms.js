1df7a439af67d6e161f4f9cdf70b3f689b8f5666
Dred0k <mybuddynoah@gmail.com>
2013-10-22 09:33:37 -0400

    Monsters_and_Realms
    more of a collection of thoughts than code ATM

---
+++ js/Monsters_and_Realms.js
@@ -0,0 +1,87 @@
+//Notes: how can we scale % with player level?
+ 
+
+//monster stats
+//Monsters can have normal advanced and named version
+//=============================================
+//low level encounters
+
+Lage Rats
+	Named Rat(Plaue Bringer)
+
+Mites
+
+
+Kobalds
+	advanced Kobald
+	Named 
+
+
+Goblins
+	advanced Goblins
+	Goblin warrior
+	Goblin shamen
+	Goblin Chief
+	
+
+BugBear
+
+
+
+//=============================================
+//medium level encounters
+
+Bandits
+
+Troll
+
+Minatour
+
+Hill Giant
+
+
+//=============================================
+//High level encounters
+
+Iron Golem
+
+
+//=============================================
+//Epic level encounters
+
+Dragons
+
+
+
+
+
+//=============================================
+// Realms
+// Realms will control the % chance to encounter monsters
+
+
+Areana // PVP Area
+
+Explore //% to find/unlock Realms
+
+
+Forest
+
+swamp
+
+Mountains
+
+Island
+
+Plains
+
+
+Dungeon // named dungeons for low - high
+
+
+
+Bandit Encampment
+
+
+//=============================================
+// encounter chace
